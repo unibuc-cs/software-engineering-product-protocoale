f7ddc6d0307d105a0e3ab51eeb72952876d80314
David Voinescu <david.voinescu20030616@gmail.com>
2024-12-25 23:25:53 +0200

    added connection to the dabase for favorite products

---
+++ Data/Migrations/20241225205220_AddFavoriteItemsTable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MDS_PROJECT.Data.Migrations
+{
+    public partial class AddFavoriteItemsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FavoriteItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Quantity = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Unit = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AddedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FavoriteItems", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FavoriteItems");
+        }
+    }
+}
---
+++ Models/FavouriteProduct.cs
@@ -0,0 +1,12 @@
+﻿namespace MDS_PROJECT.Models
+{
+    public class FavoriteProduct
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Quantity { get; set; }
+        public string Unit { get; set; }
+        public DateTime AddedDate { get; set; }
+    }
+
+}
